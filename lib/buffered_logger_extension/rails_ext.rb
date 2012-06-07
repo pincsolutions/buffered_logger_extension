8d732b71f0795886f928ce0fc597f8ecbd242bb9
Nicholas Roosevelt <nroose@gmail.com>
2012-06-07 10:35:27 -0700

    filename in log will now be our file or blank if there is no file of ours in the trace.

--- lib/buffered_logger_extension/rails_ext.rb
+++ lib/buffered_logger_extension/rails_ext.rb
@@ -5,20 +5,21 @@ ActiveSupport::BufferedLogger.class_eval do
   end
 
   def add(severity, message = nil, progname = nil, &block)
-      return if @level > severity
-      message = (message || (block && block.call) || progname).to_s
-      # If a newline is necessary then create a new message ending with a newline.
-      # Ensures that the original message is not mutated.
-      message = "#{message}\n" unless message[-1] == ?\n
-      buffer << format_message(severity, Time.now, progname, message)
-      auto_flush
-      message
+    return if @level > severity
+    message = (message || (block && block.call) || progname).to_s
+    # If a newline is necessary then create a new message ending with a newline.
+    # Ensures that the original message is not mutated.
+    message = "#{message}\n" unless message[-1] == ?\n
+    buffer << format_message(severity, Time.now, progname, message)
+    auto_flush
+    message
   end
 
   def format_message(severity, timestamp, progname, msg)
-      caller[2] =~ /^\/.*\/(.+):(\d+):/
-      stack = (severity > 1 && severity < 4) ? Kernel.caller(3).collect{|c| "   " + c}.join("\n") + "\n" : ''
-      "[#{timestamp.strftime("%Y-%m-%d %H:%M:%S %Z")}] #{level_to_s(severity)} #{$1}:#{$2} #{msg}#{stack}"
+    pattern = Regexp.new("^#{Rails.root}")
+    location = (Kernel.caller.detect{|c| c.match(pattern)} || '').split("/")[-1]
+    stack = (severity > 1 && severity < 4) ? Kernel.caller(3).collect{|c| "   " + c}.join("\n") + "\n" : ''
+    "[#{timestamp.strftime("%Y-%m-%d %H:%M:%S %Z")}] #{level_to_s(severity)} #{location} #{msg}#{stack}"
   end
 
 end
